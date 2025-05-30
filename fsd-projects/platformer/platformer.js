53773f93a87d39c252249c61236c035587d9ce80
livvv303 <craddock38301@students.apsb.org>
2025-05-30 04:57:50 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -34,27 +34,32 @@ createPlatform(150, 610, 40, 40, "hotpink");
 
 createPlatform(300, 472, 230, 20, "lightgreen");
 createPlatform(260, 471, 40, 160, "lightgreen");
+createPlatform(1240, 200, 160, 20, "hotpink")
 
 createPlatform(470, 340, 60, 20, "darkred");
 createPlatform(520, 360, 10, 130, "lightgreen");
+createPlatform(1100, 300, 50, 20, "lightgreen")
 
-createPlatform(563, 205, 40, 20, "blue");
-createPlatform(100, 280, 100, 10);
-createPlatform(100, 110, 10, 170);
+createPlatform(563, 205, 40, 20, "darkred");
+createPlatform(100, 280, 100, 10, "hotpink");
+createPlatform(100, 110, 10, 170, "hotpink");
 
 createPlatform(270, 380, 150, 5, "lightblue");
 createPlatform(270, 280, 150, 100, "lightblue");
-createPlatform(270, 280, 150, 10)
+createPlatform(270, 280, 150, 10, "hotpink");
 
-createPlatform(1200, 590, 20, 20, "hotpink");
+createPlatform(1200, 430, 50, 20, "lightgreen");
+createPlatform(700, 120, 100, 20, "darkred");
+createPlatform(1000, 550, 30, 20, "lightgreen");
     // TODO 3 - Create Collectables
-createCollectable("grace", 130, 230)
-createCollectable("steve", 1060, 100)
+createCollectable("grace", 130, 230);
+createCollectable("steve", 990, 300);
 createCollectable("diamond", 475, 420);
+createCollectable("database", 1340, 150)
     // TODO 4 - Create Cannons
 createCannon("top", 410, 1000);
-createCannon("bottom", 1150, 2300);
-createCannon("right", 400, 3400);
+createCannon("bottom", 955, 3400);
+createCannon("right", 400, 3200);
 
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
